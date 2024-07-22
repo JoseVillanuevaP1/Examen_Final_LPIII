f8aef3877c0c378cd8f07da6e8a36269ba5c037f
jose <jmvilla2301@gmail.com>
2024-07-22 15:07:04 -0500

    Villanueva Examen Final - Parte 2 - Base de Datos

--- miapp/models.py
+++ miapp/models.py
@@ -1,3 +1,16 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+class Villanueva_Persona(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    SEXO_CHOICES = [
+        ('Masculino', 'Masculino'),
+        ('Femenino', 'Femenino'),
+    ]
+    sexo = models.CharField(max_length=10, choices=SEXO_CHOICES)
+    fecha_de_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellidos}'
+
